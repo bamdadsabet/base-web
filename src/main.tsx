1d00859bb14c90809d0807fd4ebf515ac1c12db4
Amir Salehi <salehi.amir9@gmail.com>
2023-05-31 17:50:54 +0330

    grid added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { LightTheme, BaseProvider, styled, DarkTheme } from 'baseui';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const engine = new Styletron();
+const Centered = styled('div', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  height: '100%',
+});
+
+function Main() {
+  return (
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <Centered>
+          <App />
+        </Centered>
+      </BaseProvider>
+    </StyletronProvider>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
 )
